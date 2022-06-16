124999893397e48a05f98d1a9cf0f90fecb28005
lsw0604 <57swji@naver.com>
2022-06-16 19:33:27 +0900

    routerObject routing

--- my-first-next-app/pages/index.jsx
+++ my-first-next-app/pages/index.jsx
@@ -1,14 +1,25 @@
-// '/' 경로를 가지는 페이지
-import Link from "next/link";
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
 const App = () => {
+  const [name, setName] = useState("");
+  const router = useRouter();
+
   return (
-    <>
-      <h2>Link to 'potato'</h2>
-      <Link href="/vegetable/potato">
-        <a>Move to '/vegetable/potato'</a>
-      </Link>
-    </>
+    <div>
+      <button type="button" onClick={() => router.push("/tomato")}>
+        Move to tomato
+      </button>
+      <p>name</p>
+      <input 
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        style={{ marginRight: "12px" }}
+      />
+      <button type="button" onClick={() => router.push(`/vegetable/${name}`)}>
+        Move to {name}
+      </button>
+    </div>
   );
 };
 
--- my-first-next-app/pages/vegetable/[name].jsx
+++ my-first-next-app/pages/vegetable/[name].jsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const name = () => {
+  const router = useRouter();
+  console.log("router : ", router);
   return (
     <div>
       <h2>Hello</h2>
