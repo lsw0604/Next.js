e145e65df6feba52de5c24555cec610ba5e76b2f
lsw0604 <57swji@naver.com>
2022-06-16 19:47:01 +0900

    static file use

--- my-first-next-app/pages/index.jsx
+++ my-first-next-app/pages/index.jsx
@@ -1,29 +1,9 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-
 const App = () => {
-  const [name, setName] = useState("");
-  const router = useRouter();
-
   return (
     <div>
-      <button type="button" onClick={() => router.push("/tomato")}>
-        Move to tomato
-      </button>
-      <p>name</p>
-      <input 
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        style={{ marginRight: "12px" }}
-      />
-      <button type="button" onClick={() => router.push(`/vegetable/${name}`)}>
-        Move to {name}
-      </button>
-      <button type="button" onClick={() => router.push("/vegetable/carrot")}>
-        Move to Carrot
-      </button>
+      <img src="/2.jpg" alt="winter" />
     </div>
-  );
+  )  
 };
 
 export default App;
