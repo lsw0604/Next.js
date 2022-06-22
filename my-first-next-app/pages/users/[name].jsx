149e3475bce34b30e2c4013533657c2ccfe6d6da
lsw0604 <57swji@naver.com>
2022-06-22 18:25:32 +0900

    pagination CSS error

--- my-first-next-app/pages/users/[name].jsx
+++ my-first-next-app/pages/users/[name].jsx
@@ -1,69 +1,12 @@
 import fetch from "isomorphic-unfetch";
-import Profile from "../../components/Profile";
 import css from "styled-jsx/css";
-import formatDistance from "date-fns/formatDistance";
-
-/* date-fns MDN https://date-fns.org/v2.14.0/docs/formatDistance */
+import Profile from "../../components/Profile";
+import Repositories from "../../components/Repositories";
 
 const style = css`
   .user-contents-wrapper {
     display: flex;
     padding: 20px;
-  }
-  .repos-wrapper {
-    width: 100%;
-    height: 100vh;
-    overflow: scroll;
-    padding: 0px 16px;
-  }
-  .repos-header {
-    padding: 16px 0;
-    font-size: 14px;
-    font-weight: 600;
-    border-bottom: 1px solid;
-  }
-  .repos-count {
-    display: inline-block;
-    padding: 2px 5px;
-    margin-left: 6px;
-    font-size: 12px;
-    font-weight: 600;
-    line-height: 1;
-    color: #586069;
-    background-color: rgba(27, 31, 35, 0.08);
-    border-radius: 20px;
-  }
-  .repository-wrapper {
-    width: 100%;
-    border-bottom: 1px solid;
-    padding: 24px 0;
-  }
-  .repository-description {
-    padding: 12px 0;
-  }
-  a {
-    text-decoration: none;
-  }
-  .repository-name {
-    margin: 0;
-    color: #0366d6;
-    font-size: 20px;
-    display: inline-block;
-    cursor: pointer;
-  }
-  .repository-name:hover {
-    text-decoration: underline;
-  }
-  .repository-description {
-    margin: 0;
-    font-size: 14px;
-  }
-  .repository-language {
-    margin: 0;
-    font-size: 14px;
-  }
-  .repository-updated-at {
-    margin-left: 20px;
   }
 `;
 
@@ -74,32 +17,7 @@ const name = ({ user, repos }) => {
   return (
     <div className="user-contents-wrapper">
       <Profile user={user} />
-      <div className="repos-wrapper">
-        <div className="repos-header">
-          Repositories
-          <span className="repos-count">{user.public_repos}</span>
-          {user && repos && repos.map((repo) => (
-            <div className="repository-wrapper" key={repo.id}>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={`https://github.com/${user.login}/${repo.name}`}
-              >
-                <h2 className="repository-name">{repo.name}</h2>
-              </a>
-              <p className="repository-description">{repo.description}</p>
-              <p className="repository-language">
-                {repo.language}
-                <span className="repository-updated-at">
-                  {formatDistance(new Date(repo.updated_at), new Date(), {
-                    addSuffix: true,
-                  })}
-                </span>
-              </p>
-            </div>
-          ))}
-        </div>
-      </div>
+      <Repositories user={user} repos={repos} />
       <style jsx>{style}</style>
     </div>
   );
